b107f7b5087b1095f44bb341be491bfe6b0fed66
hamza <hamza.eljokh@gmail.com>
2020-10-22 12:16:34 +0200

    Version 1.0

--- ter/settings.py
+++ ter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7wu=wqbme6xv=k%8o2pp^9x!m@i5xe)1v@jb@%keh3t$($^y%_'
+SECRET_KEY = 'the Djagno secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
